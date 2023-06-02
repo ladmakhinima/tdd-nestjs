21d702cc50a5a6b3fdfb31b2c015bbd74c0085b2
cody <cody20v@gmail.com>
2023-06-02 22:13:06 +0330

    Implement TDD for category

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -3,6 +3,7 @@ import { CategoryService } from './category.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Category, CategoryDocument } from './category.entity';
 import { Model } from 'mongoose';
+import { BadRequestException, ConflictException } from '@nestjs/common';
 
 const mockCreateCategory = () => ({
   _id: 'anyid',
@@ -40,9 +41,57 @@ describe('CategoryService', () => {
     expect(service).toBeDefined();
   });
   describe('create category', () => {
-    it('create category successfully ...', () => {
-      // ERROR 
-      jest.spyOn(repository, 'create').mockReturnValueOnce(mockCreateCategory())
+    it('create category successfully ...', async () => {
+      jest
+        .spyOn(repository, 'create')
+        .mockReturnValueOnce(mockCreateCategory() as any);
+      const result = await service.create(mockCreateCategoryDTO());
+      expect(result).toEqual(mockCreateCategory());
+      expect(repository.create).toBeCalledTimes(1);
+      expect(repository.create).toBeCalledWith(mockCreateCategoryDTO());
+    });
+
+    it('create category: duplicate name error', async () => {
+      jest
+        .spyOn(service, 'create')
+        .mockReturnValueOnce(
+          new ConflictException('category duplicated by name') as any,
+        );
+
+      const result = await service.create(mockCreateCategoryDTO());
+      expect(service.create).toBeCalledWith(mockCreateCategoryDTO());
+      expect(result).toEqual(
+        new ConflictException('category duplicated by name'),
+      );
+    });
+  });
+  describe('find category', () => {
+    it('find successfully', async () => {
+      jest
+        .spyOn(repository, 'findOne')
+        .mockReturnValueOnce(mockCreateCategory() as any);
+
+      const result = await service.find({ name: mockCreateCategoryDTO().name });
+      expect(result).toEqual(mockCreateCategory());
+      expect(repository.findOne).toHaveBeenCalled();
+      expect(repository.findOne).toHaveBeenCalledTimes(1);
+      expect(repository.findOne).toHaveBeenCalledWith({
+        name: mockCreateCategoryDTO().name,
+      });
+    });
+
+    it('find : not found error', async () => {
+      jest
+        .spyOn(service, 'find')
+        .mockReturnValueOnce(
+          new BadRequestException('category is not found ...') as any,
+        );
+
+      const result = await service.find({ name: mockCreateCategoryDTO().name });
+
+      expect(result).toEqual(
+        new BadRequestException('category is not found ...') as any,
+      );
     });
   });
 });
